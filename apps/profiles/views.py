d958b4d26cf96bf0492c1c08e9083a70aee4776a
Martin Skow Røed <msroed@gmail.com>
2015-04-09 14:58:55 +0200

    Redirect and fix login page

--- apps/profiles/views.py
+++ apps/profiles/views.py
@@ -1,31 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib import auth
-from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
-from .models import Profile
-from .forms import ProfileCreationForm
 from django.contrib import messages
 
 def login_view(request):
-	if request.POST:
-		form_type = request.POST.get('form-type')
-		if form_type == 'login':
-			email = request.POST.get('email')
-			password = request.POST.get('password')
-			user = auth.authenticate(username=email, password=password)
-			if user is not None:
-				auth.login(request, user)
-				if request.GET.get('next') is not None:
-					return redirect(request.GET.get('next'))
-				return redirect(reverse('archive'))
-		elif form_type == 'register':
-			form = ProfileCreationForm(request.POST)
-			if form.is_valid():
-				profile = form.save()
-				user = auth.authenticate(username=profile.email, password=request.POST.get('password'))
-				if user is not None:
-					auth.login(request, user)
-					return redirect('/')
 	return render(request, 'profiles/login.html')
 
 @login_required
