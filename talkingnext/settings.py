ada3349a153303620965c3e2c37c07bde63c3173
Martin Skow Røed <msroed@gmail.com>
2015-04-15 16:21:39 +0200

    Read from env

--- talkingnext/settings.py
+++ talkingnext/settings.py
@@ -101,7 +101,7 @@ AUTH_USER_MODEL = 'profiles.Profile'
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": os.environ.get('REDIS_URL', 'redis://localhost:6379/10'),
+        "LOCATION": os.environ.get('REDIS_URL', ''),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
